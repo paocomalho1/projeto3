55cffeef71c1c483c7b16caf96bd0d94d2206569
ERICK <erick_honorio20@hotmail.com>
2022-03-01 21:12:01 -0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+function advice(){
+    const url = `https://api.adviceslip.com/advice`
+    fetch(url).then(
+        response => response.json()
+    ).then(
+        data => {mudarHtml(data)}
+    )
+}
+
+function mudarHtml(data){
+    console.log(data)
+    const titulo = document.querySelector(".conteudo__titulo")
+    const texto = document.querySelector(".conteudo__texto")
+    titulo.innerHTML = `advice #${data.slip.id}`
+    texto.innerHTML =  data.slip.advice
+
+}
+
+
